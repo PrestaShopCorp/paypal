a893b82935f98785225e667e4efd6824588de8dd
Thoma Biguères <tbigueres@202-ecommerce.com>
2015-04-16 10:09:42 +0200

    [*] MO - paypal : when upgrading before 3.0 http://forge.prestashop.com/browse/PNM-2275

--- upgrade/install-3.0.php
+++ upgrade/install-3.0.php
@@ -79,5 +79,15 @@ function upgrade_module_3_0($object, $install = false)
 			) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8 AUTO_INCREMENT=1');
 	}
 
+	//Add datas to paypal order
+	Db::getInstance()->Execute('
+		ALTER TABLE `' . _DB_PREFIX_ . 'paypal_order`
+		ADD `id_invoice` varchar(255) DEFAULT NULL,
+		ADD `currency` varchar(10) NOT NULL,
+		ADD `total_paid` varchar(50) NOT NULL,				
+		ADD `shipping` varchar(50) NOT NULL,
+		ADD `payment_date` varchar(50) NOT NULL,	
+	');
+
 	return true;
 }
