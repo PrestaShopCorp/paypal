ff316ff2c433293d8c335461f256461a53f655b0
Sébastien GODARD <sgodard@202-ecommerce.com>
2016-04-14 14:40:29 +0200

    [*] MO - paypal : fix error file_get_contents

--- ipn.php
+++ ipn.php
@@ -214,7 +214,18 @@ class PayPalIPN extends PayPal
             $request .= "&$key=$value";
         }
 
-        return Tools::file_get_contents($action_url.$request);
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_URL, $action_url.$request);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSLVERSION, defined('CURL_SSLVERSION_TLSv1_2') ? CURL_SSLVERSION_TLSv1_2 : 1);
+
+        $content = curl_exec($curl);
+        curl_close($curl);
+        return $content;
     }
 }
 
