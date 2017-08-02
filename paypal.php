f20f49e681a9576a9c3c1aabaaafc864ba8164b3
202 ecommerce <sgodard@202-ecommerce.com>
2017-08-02 16:28:50 +0200

    update  presto

--- paypal.php
+++ paypal.php
@@ -998,7 +998,7 @@ class PayPal extends PaymentModule
             'first_name'    => $this->context->employee->firstname,
             'last_name'     => $this->context->employee->lastname,
             'shop_name'     => Configuration::get('PS_SHOP_NAME', null, null, null, ''),
-            'ref_merchant'  => (defined(HOSTMODE)?'presto':'prestashop_')._PS_VERSION_.'_'.$this->version,
+            'ref_merchant'  => ((defined(PLATEFORM) && PLATEFORM == 'PSREAD')?'presto':'prestashop_')._PS_VERSION_.'_'.$this->version,
         );
 
         $sdk = new PaypalSDK(Configuration::get('PAYPAL_SANDBOX'));
