7b0c28195121a93467562fa5e3b14a1f1c2083cd
sgodard <sgodard@202-ecommerce.com>
2018-08-02 18:12:34 +0200

    [*] MO - paypal : delete author address

--- paypal.php
+++ paypal.php
@@ -58,7 +58,6 @@ class PayPal extends PaymentModule
         $this->name = 'paypal';
         $this->tab = 'payments_gateways';
         $this->version = '4.4.2';
-        $this->author_address = '0x416357AF746680E3B2787841AB0e51d9BB684734';
         $this->author = 'PrestaShop';
         $this->display = 'view';
         $this->module_key = '336225a5988ad434b782f2d868d7bfcd';
